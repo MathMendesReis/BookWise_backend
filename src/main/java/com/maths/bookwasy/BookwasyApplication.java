9c82250a39c7eec8695ce62879743e9acb012da4
Matheus <matheusmendesreis@outlook.com>
2024-01-20 11:20:55 -0300

    #initial docs

---
+++ src/main/java/com/maths/bookwasy/BookwasyApplication.java
@@ -0,0 +1,13 @@
+package com.maths.bookwasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwasyApplication.class, args);
+	}
+
+}
