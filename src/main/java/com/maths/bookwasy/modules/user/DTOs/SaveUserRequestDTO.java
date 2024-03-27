8741d7d38c491c2f34a2d090f3b93382aa93d1f7
Matheus <matheusmendesreis@outlook.com>
2024-03-27 19:16:08 -0300

    feat: add HATEOAS

--- src/main/java/com/maths/bookwasy/modules/user/DTOs/SaveUserRequestDTO.java
+++ src/main/java/com/maths/bookwasy/modules/user/DTOs/SaveUserRequestDTO.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SaveUserRequestDTO(
-                @Schema(example = "John Doe") @NotNull @NotBlank String name,
-                @Schema(example = "https://files.tecnoblog.net/wp-content/uploads/2020/05/avatar-de-facebook-de-mark-zuckerberg-e1589902366337-700x460.png") String avatarUrl,
-                @Schema(example = "example@google.com") @Email @NotNull @NotBlank String email,
-                @Schema(example = "true") @NotNull Boolean emailVerified) {
+        @Schema(example = "John Doe") @NotNull @NotBlank String name,
+        @Schema(example = "https://files.tecnoblog.net/wp-content/uploads/2020/05/avatar-de-facebook-de-mark-zuckerberg-e1589902366337-700x460.png") String avatarUrl,
+        @Schema(example = "example@google.com") @Email @NotNull @NotBlank String email,
+        @Schema(example = "true") @NotNull Boolean emailVerified) {
 }
--- src/main/java/com/maths/bookwasy/modules/user/controller/SaveUserController.java
+++ src/main/java/com/maths/bookwasy/modules/user/controller/SaveUserController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maths.bookwasy.modules.models.User;
 import com.maths.bookwasy.modules.user.DTOs.SaveUserRequestDTO;
-import com.maths.bookwasy.modules.user.DTOs.UserResponseDTO;
 import com.maths.bookwasy.modules.user.useCases.SaveUserUseCase;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,8 +18,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import com.maths.bookwasy.Exceptions.UserAlreadyExistsException;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 @RestController
 @RequestMapping("/user")
@@ -41,13 +37,8 @@ public class SaveUserController {
     public ResponseEntity<?> save(@Valid @RequestBody SaveUserRequestDTO saveUserRequestDTO) {
         ResponseEntity<?> response = null;
         try {
-            User user = saveUserUseCase.createUser(saveUserRequestDTO);
-            UserResponseDTO userResponseDTO = new UserResponseDTO(user);
-            EntityModel<UserResponseDTO> resource = EntityModel.of(userResponseDTO);
-            WebMvcLinkBuilder linkTo = WebMvcLinkBuilder
-                    .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).save(saveUserRequestDTO));
-            resource.add(linkTo.withRel("self"));
-            response = ResponseEntity.status(HttpStatus.CREATED).body(resource);
+            saveUserUseCase.createUser(saveUserRequestDTO);
+            response = ResponseEntity.status(HttpStatus.CREATED).body(null);
         } catch (UserAlreadyExistsException e) {
             response = ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         } catch (Exception e) {
